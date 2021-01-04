7cbfc2695995952bfffffbfa6ad16fd949f7efc9
Shanil Lobanwala <slobanwala@ad.thinkmcs.com>
2021-01-03 20:37:37 -0600

    Fixed table

--- static/js/app.js
+++ static/js/app.js
@@ -2,3 +2,18 @@
 var tableData = data;
 
 // YOUR CODE HERE!
+// use tbody tag to group body content
+tbody = d3.select("tbody")
+
+function updateTable(data) {
+  tbody.text("")
+
+  data.forEach(function(alien_sighting) {
+    updated_tRow = tbody.append("tr")
+    Object.entries(alien_sighting).forEach(function([key, value]) {
+      updated_tData = updated_tRow.append("td").text(value)
+    })
+  })
+}
+
+updateTable(tableData)

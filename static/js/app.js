d141be629c7f599808c2c8503ad3dbf6cbea534a
Shanil Lobanwala <slobanwala@ad.thinkmcs.com>
2021-03-04 13:07:50 -0600

    Fiddling with filter

--- static/js/app.js
+++ static/js/app.js
@@ -6,14 +6,40 @@ var tableData = data;
 tbody = d3.select("tbody")
 
 function updateTable(data) {
-  tbody.text("")
+  // console.log(data);
+  tbody.text("");
 
   data.forEach(function(alien_sighting) {
     updated_tRow = tbody.append("tr")
     Object.entries(alien_sighting).forEach(function([key, value]) {
-      updated_tData = updated_tRow.append("td").text(value)
+      updated_tData = updated_tRow.append("td").text(value);
     })
   })
 }
 
-updateTable(tableData)
+function filterData(data, htmlName, fieldName) {
+  return tableData.filter(ufo_sighting => {
+    console.log(ufo_sighting.htmlName);
+    console.log(data.property("value"));
+    ufo_sighting.htmlName === data.property("value") || !data.property("value");
+  })
+}
+
+updateTable(tableData);
+
+// filter/submit button logic
+var filterButton = d3.select("#filter-btn");
+
+filterButton.on("click", function() {
+  // We don't want page to refresh instead we want data to update.
+  d3.event.preventDefault();
+
+  var dateEntered = d3.select("#datetime");
+  var countryEntered = d3.select("#country");
+  var stateEntered = d3.select("#state");
+  var cityEntered = d3.select("#city");
+  var ufoTypeEntered = d3.select("#shape");
+
+  filteredField = filterData(dateEntered, "datetime", "dateEntered");
+  updateTable(filteredField);
+})
